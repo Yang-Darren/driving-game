75251a8737048af42a196029098ca4a23b8eadee
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-05-25 01:54:41 +0000

    Added function for when down arrow is pushed and car facing down, it will move down.

--- js/main.js
+++ js/main.js
@@ -64,5 +64,13 @@ function startCar() {
     var newValue = movingCar;
     car.position.y = newValue;
     $carImg.style.bottom = (newValue + 'px');
+  } else if (car.direction === 'down') {
+    var movingCar = parseInt($carImg.style.top) + 15;
+    if (isNaN(movingCar)) {
+      movingCar = 15;
+    }
+    var newValue = movingCar;
+    car.position.y = newValue;
+    $carImg.style.top = (newValue + 'px');
   }
 }
